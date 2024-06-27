8ba31b3261b93a3f036bab7906e3589d681262a0
Mahi-1703 <148611061+Mahi-1703@users.noreply.github.com>
2024-06-27 13:29:10 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,52 @@
+//syntax
+function name() {
+console.log("N");
+  console.log("I");
+  console.log("E");
+  console.log("L");
+  console.log("I");
+  console.log("T");
+}
+name()
+name()
+let a
+ function add(num1,num2) {
+   console.log(num1+num2);
+ }
+add(2,3)
+ function add(num1,num2) {
+   console.log(num1+num2);
+let result = num1 + num2
+console.log("nielit");
+return result
+console.log("nielit");
+ }
+function add(number1, number2){
+return number1 + number2
+}
+const res =add(2,3)
+console.log(res);
+function login(user="Guest") {
+ if (user === undefined){
+ console.log("Please provide a valid user");
+return
+}
+return `${user} Just logged in`}
+ console.log( login());
+function price(num,num2,...num1) {
+return num1
+}
+console.log(price(100,200,300,400,500,2000));
+ const user ={
+name:"nielit",
+age:25
+ }
+const user = ["NIELIT",20]
+function obj(anyArr) {
+    return anyArr[0]
+}
+console.log(obj(user));
+const fun = ()=>{
+    return "Hello"
+}
+console.log(fun());
