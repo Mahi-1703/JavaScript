de355d6183d5fd9991d28eaf163be61446566614
Mahi-1703 <148611061+Mahi-1703@users.noreply.github.com>
2024-06-27 13:47:36 +0530

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -3,39 +3,40 @@
 //1)Primitive datatypes
 //1.number => 1,2,3... //2.string => 'helo',"helo",`hello` //3.boolean => true,false
 //4.undefined => 
-   // let a
-//console.log(a);//
+   let a
+console.log(a);
 //5.null => standalone value
-//let b= null
-//console.log(b);//
+let b= null
+console.log(b);
 //6.symbol => unique value//7.bigint => very large number
 
 //2)Non-Primitive datatypes(reference data type)
 //1.object //2.array //3.function
-//let user={ 
-    //name:"NIELIT",
-    //age:24,
-   // isAdmin:true,
-   // fullName: { }}//
-//let arr=["html",22,]
-//console.log(typeof user);
-//console.log(typeof arr);
+let user={ 
+    name:"NIELIT",
+    age:24,
+   isAdmin:true,
+   fullName: { }
+}
+let arr=["html",22,]
+console.log(typeof user);
+console.log(typeof arr);
 //***************************************** */
-//let age="24";
-//let logIn=true;
-//console.log(typeof logIn);
-//console.log(typeof(age));
+let age="24";
+let logIn=true;
+console.log(typeof logIn);
+console.log(typeof(age));
 // to convert string into number
-//let num=String(age)
-//console.log(typeof num);//
+let num=String(age)
+console.log(typeof num);//
 
 //Operators
-//console.log(2>1);
-//console.log(2<1);//
-//console.log(2==1);//
+console.log(2>1);
+console.log(2<1);//
+console.log(2==1);//
 // >,<,,>=,<=,===,!=,!==
 //===will check both value and datatype
-//console.log("2"=== 2);//
+console.log("2"=== 2);//
 
 //Datatypes have 2 types of memory:-
 //stack and heap
