7925316bf9c38bcf1d70e0eb7a329da567c2649d
Mahi-1703 <148611061+Mahi-1703@users.noreply.github.com>
2024-06-27 13:13:47 +0530

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,19 @@
+// we assign the variable in 3 methods//
+//const --> we can not reassign the value//
+// let --> we can reassign the value//
+// var --> we can reassign the value//
+
+const id= 1234;
+let email ="nielit@gmail.com";
+var password ='123456';
+country ="India";
+//const -> we cant reassign the value//
+ id ="4321";
+ email="nielitchennai@gmail.com";
+ password="1234";
+// country="USA";//
+  console.table([id,email,password,country]);
+//scope
+{
+    email=""
+}
