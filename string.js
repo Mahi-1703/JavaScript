0a5a86add1e52cc4a42ecdc5769becddc8253013
Mahi-1703 <148611061+Mahi-1703@users.noreply.github.com>
2024-06-27 13:15:32 +0530

    Create string.js

---
+++ string.js
@@ -0,0 +1,31 @@
+const name="nielitChennai"
+//console.log(name);
+//const anotherName=new String("hello World")
+//console.log(anotherName);
+//console.log(name+"Chennai"+anotherName+"");
+//console.log(`hello name is ${name} and my message is ${anotherName}`);//
+//console.log(name.length);
+//console.log(name.__proto__);
+//console.log(name.toLowerCase());
+//console.log(name.toUpperCase());
+//console.log(name.charAt('2'));
+//console.log(name.indexOf('i'));//
+
+//nummbers and math
+const balance=200;
+console.log(typeof balance);
+const score = new Number(2000);
+console.log(score);
+//console.log(score.toString().length);
+//console.log(score.toFixed(2));
+//console.log(score.toPrecision(2));//
+//const num=100000000
+//console.log(num.toPrecision(3));//
+//console.log(num.toLocaleString());//
+
+//console.log(Math);
+//console.log(Math.abs(-5));//
+//console.log(Math.round(5.4));//
+//console.log(Math.floor(4.9));//
+//console.log(Math.ceil(4.1));//
+console.log(Math.floor(Math.random()*10)+1);
